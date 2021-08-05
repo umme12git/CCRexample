0d26a3b656358b16474ba3c023c06a4e4df2910d
umme <unnipa01@yahoo.com>
2021-08-05 07:41:16 -0500

    initial copy

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CCRexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = "";
+
+            response = (answer < 9) ? $"{answer} is less then 9" : $"{answer} is more then or equal to 9";
+
+            Console.WriteLine(response);
+
+        }
+    }
+}
